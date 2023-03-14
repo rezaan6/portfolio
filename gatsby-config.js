ae93dbad4fce7b4dd59c2b0aa9cfcb459c3081cc
rezaan6 <rezaan6@gmail.com>
2023-03-14 21:59:36 +0530

    update 7 files and delete 1 file

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Rezaan Riyaz',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+      'Rezaan Riyaz is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    siteUrl: 'https://portfolio-rezaan6.vercel.app/', // No trailing slash allowed!
+    image: 'src/images/logo.png', // Path to your image you placed in the 'static' folder
+    twitterUsername: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Rezaan Riyaz',
+        short_name: 'Rezaan Riyaz',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
